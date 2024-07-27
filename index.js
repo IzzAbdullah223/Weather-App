e7f89fb6cd43984ac5acb5184b1597034a888c61
Izz Abdallah <x3zoabdullah@gmail.com>
2024-07-27 17:24:22 +0400

    .

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ async function getWeatherData(City){
 
 
 function WeatherInfoLeft(Data){
+    console.log(Data)
   
   placeName.textContent=Data.resolvedAddress
   weatherStatus.textContent=Data.currentConditions.conditions
@@ -65,25 +66,30 @@ function WeatherInfoLeft(Data){
 }
 
 function weatherInfoRightFirstBox(Data){
+    const date = new Date(Data.days[1].datetime)
+    let Time =getDateForBoxes(date)
+   document.querySelector(".firstBoxTitle").textContent=`${Time} ${date.getDate()}`
    document.querySelector(".firstBoxWeather").textContent=Data.days[1].conditions
    document.querySelector(".firstBoxImage").src = firstBoxImage(Data)
    const farenhit=Data.days[1].temp
-   console.log(farenhit)
    document.querySelector(".firstBoxTemp").textContent=`${((farenhit-32)*5/9).toFixed(1)}°C`
-   document.querySelector(".RainNumberBox1").textContent=Data.days[1].precip+"%"
+   document.querySelector(".RainNumberBox1").textContent=`Chances of rain: ${Data.days[1].precip}%`
 
 }
 
 function weatherInfoRightSecondBox(Data){
-    console.log(Data)
-   document.querySelector(".secondBoxWeather").textContent=Data.days[1].conditions
-   document.querySelector(".secondBoxImage").src = firstBoxImage(Data)
-   const farenhit=Data.days[1].temp
-   console.log(farenhit)
+    const date = new Date(Data.days[2].datetime)
+    let Time =getDateForBoxes(date)
+   document.querySelector(".secondBoxTitle").textContent=`${Time} ${date.getDate()}`
+   document.querySelector(".secondBoxWeather").textContent=Data.days[2].conditions
+   document.querySelector(".secondBoxImage").src = secondBoxImage(Data)
+   const farenhit=Data.days[2].temp
    document.querySelector(".secondBoxTemp").textContent=`${((farenhit-32)*5/9).toFixed(1)}°C`
-   document.querySelector(".RainNumberBox2").textContent=Data.days[1].precip+"%"
+   document.querySelector(".RainNumberBox2").textContent=`Chances of rain: ${Data.days[2].precip}%`
 
 }
+
+
 function weatherImage(Data){
    switch(true) {
         case Data.currentConditions.conditions==="Clear":
@@ -91,6 +97,15 @@ function weatherImage(Data){
 
         case Data.currentConditions.conditions==="Partially cloudy":
             return "./PartiallyCloudy.png";
+
+        case Data.currentConditions.conditions==="Overcast":
+            return "./overcast.png";
+
+        case Data.currentConditions.conditions==="Rain, Partially cloudy":
+            return "./PartialRain.png"
+
+        case Data.currentConditions.conditions==="Rain, Overcast":
+            return "./OvercastRain.png"
    }
 
 }
@@ -102,8 +117,106 @@ function firstBoxImage(Data){
 
         case Data.days[1].conditions==="Partially cloudy":
             return "./PartiallyCloudy.png";
+
+        case Data.days[1].conditions==="Overcast":
+            return "./overcast.png";
+
+        case Data.days[1].conditions==="Rain, Partially cloudy":
+            return "./PartialRain.png";
+
+        case Data.currentConditions[1]==="Rain, Overcast":
+            return "./OvercastRain.png"
    }
 
 
 }
+
+function secondBoxImage(Data){
+    switch(true) {
+        case Data.days[2].conditions==="Clear":
+            return "./sun.png";
+
+        case Data.days[2].conditions==="Partially cloudy":
+            return "./PartiallyCloudy.png";
+
+        case Data.days[2].conditions==="Overcast":
+            return "./overcast.png";
+
+        case Data.days[2].conditions==="Rain, Partially cloudy":
+            return "./PartialRain.png";
+
+        case Data.days[2].conditions==="Rain, Overcast":
+            return "./OvercastRain.png"
+   }
+}
+
+function getDateForBoxes(date){
+    let Day;
+    let Month;
+    
+    switch(true){
+        
+        case date.getDay()===0:
+            Day="Sunday";
+            break;
+
+        case date.getDay()===1:
+            Day="Monday"
+            break;
+
+        case date.getDay()===2:
+            Day="Tuesday"
+            break;
+
+        case date.getDay()===3:
+            Day="Wenseday"
+            break;
+
+        case date.getDay()===4:
+            Day="Thursday"
+            break;
+
+        case date.getDay()===5:
+            Day="Friday"
+            break;
+
+        case date.getDay()===6:
+            Day="Saturday";
+            break;     
+    }
+
+    switch(true){
+        case date.getMonth()===0:
+            Month="January";
+            break;
+
+        case date.getMonth()===1:
+            Month="Feburary"
+            break;
+
+        case date.getMonth()===2:
+            Month="March"
+            break;
+
+        case date.getMonth()===3:
+            Month="April"
+            break;
+
+        case date.getMonth()===4:
+            Month="May"
+            break;
+
+        case date.getMonth()===5:
+            Month="June"
+            break;
+
+        case date.getMonth()===6:
+            Month="July";
+            break;     
+    }
+
+    return `${Day}, ${Month}`
+
+}
+ 
  
